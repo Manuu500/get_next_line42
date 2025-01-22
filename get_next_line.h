6389d89b7fba6800d78929203146b6f5372b6139
mruiz-ur <espartanmanu1122@gmail.com>
2025-01-22 11:47:45 +0100

    changes

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mruiz-ur <mruiz-ur@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 17:23:21 by mruiz-ur          #+#    #+#             */
-/*   Updated: 2025/01/22 11:26:37 by mruiz-ur         ###   ########.fr       */
+/*   Updated: 2025/01/22 11:47:32 by mruiz-ur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,8 @@
 # include <fcntl.h>
 
 char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	*ft_memcpy(void *dest, const void *src, unsigned int n);
-void	*ft_calloc(size_t num, size_t size);
-void	ft_bzero(void *s, unsigned int leng);
 
 #endif
